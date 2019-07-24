e129126fe7a15090fe53dac4914a01ce5c149ba2
Jihadist <tomasiche@gmail.com>
2019-07-24 22:48:30 +0300

    Add files

---
+++ VectorTemplate/VectorTemplate.cpp
@@ -0,0 +1,98 @@
+﻿// VectorTemplate.cpp: определяет точку входа для приложения.
+//
+
+#include "VectorTemplate.h"
+
+using namespace std;
+
+template<typename T>
+void print(T& arr)
+{
+	std::cout << "(" << typeid(T).name() << "):{";
+	for (auto& i : arr)
+		std::cout << "'" << i << "',";
+	std::cout << "}" << std::endl;
+}
+
+int main()
+{
+	Vector<int> vec;
+	vec.push_back(13);
+	vec.push_back(1448);
+	vec.push_back(15);
+	print(vec);
+	Vector<char> Str;
+	Str.push_back('B');
+	Str.push_back('A');
+	Str.push_back('D');
+	print(Str);
+	cout << "Hello CMake." << endl;
+	return 0;
+}
+
+
+template<typename T>
+Vector<T>::Vector(const Vector& s)
+{
+	auto newdata = alloc_n_copy(s.begin(), s.end());
+	elements = newdata.first;
+	first_free = cap = newdata.second;
+}
+
+template<typename T>
+Vector<T>& Vector<T>::operator=(const Vector& rhs)
+{
+	auto data = alloc_n_copy(rhs.begin(), rhs.end());
+	free();
+	elements = data.first;
+	first_free = cap = data.second;
+	return *this;
+}
+
+template<typename T>
+Vector<T>::~Vector()
+{
+	free();
+}
+
+template<typename T>
+void Vector<T>::push_back(const T& s)
+{
+	chk_n_alloc();
+	alloc.construct(first_free++, s);
+}
+
+
+
+template<typename T>
+std::pair<T*, T*> Vector<T>::alloc_n_copy(const T* b, const T* e)
+{
+	auto data = alloc.allocate(b - e);
+	return { data,uninitialized_copy(b,e,data) };
+}
+
+template<typename T>
+void Vector<T>::free()
+{
+	if(elements)
+	{
+		for (auto p = first_free; p != elements; )
+			alloc.destroy(--p);
+		alloc.deallocate(elements, cap - elements);
+	}
+}
+
+template<typename T>
+void Vector<T>::reallocate()
+{
+	auto newcapacity = size() ? 2 * size() : 1;
+	auto newdata = alloc.allocate(newcapacity);
+	auto dest = newdata;
+	auto elem = elements;
+	for (size_t i = 0; i != size(); ++i)
+		alloc.construct(dest++, std::move(*elem++));
+	free();
+	elements = newdata;
+	first_free = dest;
+	cap = elements + newcapacity;
+}
---
+++ VectorTemplate/VectorTemplate.h
@@ -0,0 +1,38 @@
+﻿// VectorTemplate.h : включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта.
+
+#pragma once
+
+#include <iostream>
+#include <memory>
+template<typename T>
+class Vector
+{
+
+public:
+	Vector():elements(nullptr),first_free(nullptr),cap(nullptr){}
+	Vector(const Vector&);
+	Vector& operator=(const Vector&);
+	~Vector();
+	void push_back(const T&);
+	size_t size() const { return first_free - elements; }
+	size_t capacity() const { return cap - elements; }
+
+	T* begin() const { return elements; }
+	T* end() const { return first_free; }
+private:
+	std::allocator<T> alloc;
+	void chk_n_alloc()
+	{
+		if(size()==capacity()) reallocate();
+	}
+	std::pair<T*, T*> alloc_n_copy (const T*, const T*);
+	void free();
+	void reallocate();
+	T* elements;
+	T* first_free;
+	T* cap;
+};
+
+
+// TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
